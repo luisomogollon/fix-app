120a1d3bedf6e3050e2ebc7424fb34c53a254663
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2022-09-23 13:39:19 -0400

    cambio

--- src/App.jsx
+++ src/App.jsx
@@ -18,46 +18,29 @@ function App() {
   }
   return (
     <section className="text-gray-600 body-font bg-gray-100">
-      <section class="w-full px-8 text-gray-700 bg-slate-400">
-        <div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
-          <div class="relative flex flex-col md:flex-row">
+      <section className="w-full px-8 text-gray-700 bg-slate-400">
+        <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-7xl">
+          <div className="relative flex flex-col md:flex-row">
             <a
               href="#_"
-              class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0"
+              className="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0"
             >
-              <span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">
-                L'Art<span class="text-indigo-600">.</span>
+              <span className="mx-auto text-xl font-black leading-none text-gray-900 select-none">
+                L'Art<span className="text-indigo-600">.</span>
               </span>
             </a>
-            <nav class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
+            <nav className="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
               <a
                 href="#_"
-                class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
+                className="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
               >
                 Home
               </a>
-              <a
-                href="#_"
-                class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
-              >
-                Features
-              </a>
-              <a
-                href="#_"
-                class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
-              >
-                Pricing
-              </a>
-              <a
-                href="#_"
-                class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900"
-              >
-                Blog
-              </a>
+             
             </nav>
           </div>
 
-          <div class="inline-flex items-center ml-5 space-x-6 lg:justify-end"></div>
+          <div className="inline-flex items-center ml-5 space-x-6 lg:justify-end"></div>
         </div>
       </section>
 
